e572cef4d4b307e6e8e9d2b268d880d11130290b
Vardan <vardan.vanyan@gmail.com>
2025-04-21 16:05:53 -0700

    added basic folder structure

---
+++ ryddo-frontend/components/layout/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeaderNavigation from './HeaderNavigation';
+export default function Header() {
+  return (
+    <header>
+      Header
+      <HeaderNavigation />
+    </header>
+  );
+}
