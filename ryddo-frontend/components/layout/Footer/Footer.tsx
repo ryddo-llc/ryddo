841872a76496a8fa88ce1d2661c7ca996c177466
Vardan <vardan.vanyan@gmail.com>
2025-04-21 16:11:41 -0700

    changed header and footer into layout for better separation

---
+++ ryddo-frontend/components/layout/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FooterNavigation from './FooterNavigation';
+import SocialLinks from './SocialLinks';
+export default function Footer() {
+  return (
+    <footer>
+      Footer
+      <FooterNavigation />
+      <SocialLinks />
+    </footer>
+  );
+}
---
+++ ryddo-frontend/components/layout/Footer/SocialLinks.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SocialLinks() {
+  return <div>Footer Social Links</div>;
+}
