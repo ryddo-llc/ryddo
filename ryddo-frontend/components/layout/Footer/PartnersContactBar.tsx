b421ed4d11727935eff713a18673dab9d8de7815
Vardan <vardan.vanyan@gmail.com>
2025-04-23 14:17:18 -0700

    added logos to contactbar

--- ryddo-frontend/components/layout/Footer/PartnersContactBar.tsx
+++ ryddo-frontend/components/layout/Footer/PartnersContactBar.tsx
@@ -1,5 +1,9 @@
 import Link from 'next/link';
 import React from 'react';
+import Image from 'next/image';
+import cake from '../../../public/images/cake-logo.svg';
+import super73 from '../../../public/images/super-73-logo.svg';
+import minimotors from '../../../public/images/Minimotors-logo1.svg';
 
 type BrandProps = {
   name: string;
@@ -8,32 +12,32 @@ type BrandProps = {
 
 export default function PartnersContactBar() {
   const brands = [
-    { name: 'Super73', image: '/' },
-    { name: 'Cake', image: '/' },
-    { name: 'MiniMotors', image: '/' },
+    { name: 'Super73', image: cake },
+    { name: 'Cake', image: super73 },
+    { name: 'MiniMotors', image: minimotors },
   ];
   return (
     <div className='bg-[#333333] flex flex-col md:flex-row text-white items-center font-bold'>
-      <div className='h-16 flex items-center border-r border-white px-6 md:px-10'>
+      <div className='h-16 flex items-center border-r border-[#FFFFFF] px-6 md:px-10'>
         <Link href='/'>Sign up for Free ryddo adventures 2</Link>
       </div>
 
-      <div className='flex-1 flex justify-center gap-4 md:gap-32 py-4 md:py-0'>
+      <div className='flex-1 flex justify-center items-center gap-4 md:gap-32 py-4 md:py-0'>
         {brands.map((brand: BrandProps) => (
-          <Link key={brand.name} href={brand.image}>
-            {brand.name}
+          <Link key={brand.name} href='/products'>
+            <Image src={brand.image} width={124} height={30} alt={brand.name} />
           </Link>
         ))}
       </div>
 
       <div className='flex w-full md:w-auto'>
-        <div className='h-16 flex items-center justify-center border-l border-white w-1/2 md:w-60'>
+        <div className='h-16 flex items-center justify-center border-l border-[#FFFFFF] w-1/2 md:w-60'>
           <Link href='/'>323.676.7433</Link>
         </div>
 
         <Link
           href='/service'
-          className='bg-[#F92F7B] h-16 flex items-center justify-center w-1/2 md:w-60 border-l border-white'
+          className='bg-[#F92F7B] h-16 flex items-center justify-center w-1/2 md:w-60 border-l border-[#F92F7B]'
         >
           Book Now
         </Link>
