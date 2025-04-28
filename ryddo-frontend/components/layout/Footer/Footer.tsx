7afebb21fa805dbed6f2b380ce37b1049c803bcb
Vardan <vardan.vanyan@gmail.com>
2025-04-28 11:27:40 -0700

    added aria-lables and Seo optimzations for footer

--- ryddo-frontend/components/layout/Footer/Footer.tsx
+++ ryddo-frontend/components/layout/Footer/Footer.tsx
@@ -8,16 +8,25 @@ import Copyright from './Copyright';
 
 export default function Footer() {
   return (
-    <footer className='bg-[#FFFFFF] flex flex-col justify-center items-center text-black text-xs'>
-      <div className='flex gap-40 pl-32 pr-32 border-b border-b-gray-300 pb-12'>
-        <InfoSection />
-        <ShopLinks />
-        <ExploreLinks />
-        <ContactSection />
-      </div>
-      <div>
-        <Copyright />
-      </div>
-    </footer>
+    <>
+      <footer
+        className='bg-[#FFFFFF] flex flex-col justify-center items-center text-black text-xs'
+        role='contentinfo'
+        aria-label='Site Footer'
+      >
+        <nav
+          className='flex gap-40 pl-32 pr-32 border-b border-b-gray-300 pb-12'
+          aria-label='Footer Navigation'
+        >
+          <InfoSection />
+          <ShopLinks />
+          <ExploreLinks />
+          <ContactSection />
+        </nav>
+        <div>
+          <Copyright />
+        </div>
+      </footer>
+    </>
   );
 }
