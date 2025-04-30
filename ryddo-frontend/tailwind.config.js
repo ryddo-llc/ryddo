fe82ffc3740b259f79f0a262baf453ec89c103e1
Vardan <vardan.vanyan@gmail.com>
2025-04-30 14:33:32 -0700

    fixed incorrect nesting of fontFamily in tailwind config

--- ryddo-frontend/tailwind.config.js
+++ ryddo-frontend/tailwind.config.js
@@ -30,9 +30,9 @@ module.exports = {
           '0%': { transform: 'translateY(-20px)', opacity: '0' },
           '100%': { transform: 'translateY(0)', opacity: '1' },
         },
-        fontFamily: {
-          nunito: ['var(--font-nunito)', ...fontFamily.sans],
-        },
+      },
+      fontFamily: {
+        nunito: ['var(--font-nunito)', ...fontFamily.sans],
       },
     },
     plugins: [],
