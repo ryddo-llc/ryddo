d6ce6b2b52cb95e6eb076f6c71d6538b16646ba0
Vardan <vardan.vanyan@gmail.com>
2025-04-30 14:38:57 -0700

    fixed issue with onClic

--- ryddo-frontend/components/ui/Button.tsx
+++ ryddo-frontend/components/ui/Button.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 type ButtonProps = {
   text?: string;
   link?: string;
-  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick?: () => void;
   className?: string;
   width?: string;
   height?: string;
@@ -32,6 +32,7 @@ const Button = ({
   return (
     <Link
       href={link}
+      onClick={onClick}
       className={`${width} ${height} ${bgColor} ${rounded} ${shadow} ${textColor} ${fontSize} ${fontWeight} font-['Inter'] tracking-wide flex items-center justify-center ${className} ${
         disabled ? 'opacity-50 cursor-not-allowed' : ''
       }`}
