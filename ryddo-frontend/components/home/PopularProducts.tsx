5110571951e7166386bb56ff71eff8e690d07ced
Vardan <vardan.vanyan@gmail.com>
2025-05-07 16:01:43 -0700

    create Propular Products page and added some styling

--- ryddo-frontend/components/home/PopularProducts.tsx
+++ ryddo-frontend/components/home/PopularProducts.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
-
+import porducts from '../../lib/data/products';
 export default function PopularProducts() {
-  return <section>Popular Products</section>;
+  return (
+    <section
+      aria-label='Popular Products'
+      className='w-full h-[45vh] md:h-[50vh] lg:h-[65vh] p-10'
+    >
+      <div className='relative pr-20 pl-20 pt-10'>
+        <div className=' p-4 inline-block'>
+          <h2 className='text-5xl text-zinc-800 font-extrabold font-["Nunito"] leading-[57.60px]'>
+            Popular Products
+            <span className='w-4 h-16 justify-center text-[#F92F7B] text-6xl font-bold leading-9'>
+              .
+            </span>
+          </h2>
+          <p className='text-zinc-800 text-md font-semibold leading-loose'>
+            View and learn more about our lastest products.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
---
+++ ryddo-frontend/lib/data/products.ts
@@ -0,0 +1,53 @@
+const products = [
+  {
+    id: 1,
+    name: 'Dualtron Thunder',
+    image: '/images/dualtron-thunder.jpg',
+    originalPrice: '$3,800',
+    salePrice: '$3,499',
+  },
+  {
+    id: 2,
+    name: 'Super73-RX',
+    image: '/images/super73-rx.jpg',
+    price: '$3,695',
+  },
+  {
+    id: 3,
+    name: 'Super73-S1',
+    image: '/images/super73-s1.jpg',
+    price: '$1,995',
+  },
+  {
+    id: 4,
+    name: 'Eagle Pro',
+    image: '/images/eagle-pro.jpg',
+    priceRange: '$2,290 - $2,640',
+  },
+  {
+    id: 5,
+    name: 'Super73-Z1',
+    image: '/images/super73-z1.jpg',
+    price: '$1,395',
+  },
+  {
+    id: 6,
+    name: 'Super73-S2',
+    image: '/images/super73-s2.jpg',
+    price: '$2,995',
+  },
+  {
+    id: 7,
+    name: 'Super73 R',
+    image: '/images/super73-r.jpg',
+    price: '$3,295',
+  },
+  {
+    id: 8,
+    name: '2X2 Adventure Bike',
+    image: '/images/2x2-adventure-bike.jpg',
+    priceRange: '$6,999 - $7,999',
+  },
+];
+
+export default products;
