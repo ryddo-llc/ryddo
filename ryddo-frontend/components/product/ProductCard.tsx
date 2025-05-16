76f83915a22bb30fd7eb532310c89932829bf4b9
Vardan <vardan.vanyan@gmail.com>
2025-05-16 11:34:54 -0700

    wrapped product card in article for better SEO

--- ryddo-frontend/components/product/ProductCard.tsx
+++ ryddo-frontend/components/product/ProductCard.tsx
@@ -17,36 +17,38 @@ interface ProductCardProps {
 
 export default function ProductCard({ product }: ProductCardProps) {
   return (
-    <Link
-      href={`/prdocucts/${product.type}/${product.id}`}
-      className='flex flex-col items-center mb-8'
-    >
-      {/* Product Image */}
-      <div className='rounded-[10px] overflow-hidden'>
-        <Image
-          src={product.image}
-          alt={product.name}
-          width={300}
-          height={300}
-          className='w-full h-full object-cover '
-          priority
-        />
-      </div>
+    <article className='flex flex-col items-center mb-8'>
+      <Link
+        href={`/prdocucts/${product.type}/${product.id}`}
+        title={`View details for ${product.name}`}
+      >
+        {/* Product Image */}
+        <div className='rounded-[10px] overflow-hidden'>
+          <Image
+            src={product.image}
+            alt={`${product.name} || 'Product image'}`}
+            width={300}
+            height={300}
+            className='w-full h-full object-cover '
+            priority
+          />
+        </div>
 
-      {/* Product Info */}
-      <h3 className='text-center text-zinc-800 text-sm font-bold mb-2 pt-2 leading-none'>
-        {product.name}
-      </h3>
-      <div className='text-center mb-4'>
-        {product.originalPrice && (
-          <span className='line-through text-neutral-500 text-base font-semibold mr-2'>
-            {product.originalPrice}
-          </span>
-        )}
-        {product.salePrice && <span>{product.salePrice}</span>}
-        {product.price && <span>{product.price}</span>}
-        {product.priceRange && <span>{product.priceRange}</span>}
-      </div>
-    </Link>
+        {/* Product Info */}
+        <h2 className='text-center text-zinc-800 text-sm font-bold mb-2 pt-2 leading-none'>
+          {product.name}
+        </h2>
+        <div className='text-center mb-4'>
+          {product.originalPrice && (
+            <span className='line-through text-neutral-500 text-base font-semibold mr-2'>
+              {product.originalPrice}
+            </span>
+          )}
+          {product.salePrice && <span>{product.salePrice}</span>}
+          {product.price && <span>{product.price}</span>}
+          {product.priceRange && <span>{product.priceRange}</span>}
+        </div>
+      </Link>
+    </article>
   );
 }
