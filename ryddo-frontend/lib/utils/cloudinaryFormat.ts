eaa7caf0767436fe54b53a71badf98016f0958e2
Vardan <vardan.vanyan@gmail.com>
2025-06-24 15:20:02 -0700

    added some basic input validation to cloudinaryFormat function

--- ryddo-frontend/lib/utils/cloudinaryFormat.ts
+++ ryddo-frontend/lib/utils/cloudinaryFormat.ts
@@ -1,5 +1,12 @@
 import { getCldImageUrl } from 'next-cloudinary';
 export default function cloudinaryFormat(name: string): string {
+  if (!name) {
+    throw new Error('Invalid Input: name must be non-empty');
+  }
+
+  if (name.trim().length === 0) {
+    throw new Error('Invalid input: name cannot be empty');
+  }
   //making sure name matches the same format its in the cloudinary CDN
   const cloudinaryFormattedName = name
     .replace(/\s+/g, '-')
